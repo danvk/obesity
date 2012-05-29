97aac0d4d904a9e81a059e91cd25b39c4b042f9e
Max Shron <max.shron@gmail.com>
2012-05-29 16:39:39 -0400

    Reads in an xpt file and puts out a JSON dict.

---
+++ xpt_to_json.R
@@ -0,0 +1,10 @@
+# Read in an xpt file and produce a line-by-line JSON result
+
+args <- commandArgs(trailingOnly = TRUE)
+
+library('foreign');
+library('RJSONIO');
+print(' [*] Reading data...')
+d <- read.xport(file=args[1])
+
+cat(toJSON(unname(apply(d, 1, function(x) as.data.frame(t(x))))))
