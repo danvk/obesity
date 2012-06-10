70e68e7c29941d755fdec1c7c2355847d676077c
Max Shron <max.shron@gmail.com>
2012-06-09 21:03:09 -0400

    Decile rather than median

--- mean_and_decile.py
+++ mean_and_decile.py
@@ -5,8 +5,8 @@ Generates tabbed data on mean & median BMI broken down by year and gender.
 
 import brfss
 
-def ExtractMedian(values_weights, total_weight=None):
-    """Returns the median value given an unordered (value, weight) array.
+def ExtractDeciles(values_weights, total_weight=None):
+    """Returns the deciles given an unordered (value, weight) array.
     
     Side effect: Sorts the array.
     """
@@ -16,11 +16,19 @@ def ExtractMedian(values_weights, total_weight=None):
     values_weights.sort()
 
     tally = 0.0
+    out = []
     for v, w in values_weights:
         tally += w
-        # TODO(danvk): handle in-between case
-        if 2 * tally >= total_weight:
-            return v
+        for i in xrange(len(out),11):
+            # small adjustment for summation error
+            if tally+.0001 >= total_weight * (i*.1):
+                out.append(v)
+    try:
+        assert len(out) == 11
+    except:
+        print(out,tally,total_weight,values_weights[:10])
+        raise IndexError
+    return out
 
 
 def ExtractMean(values_weights, total_weight=None):
@@ -32,8 +40,11 @@ def ExtractMean(values_weights, total_weight=None):
 
 
 def main():
-    print '\tBoth\t\tMale\t\tFemale\t'
-    print 'Year\tMean\tMedian\tMean\tMedian\tMean\tMedian'
+    header = ['year']
+    for t in ['Both','Male','Female']:
+        for k in ['Mean'] + ['%ith_pctile'%(i*10) for i in xrange(1,10)]:
+            header.append("%s-%s"%(t,k))
+    print '\t'.join(header)
     for year, rec_it in brfss.RecordsByYear():
         # compute mean & median per-year
         bmis = [[], []]  # (bmi, record_weight) for [male, female]
@@ -48,19 +59,21 @@ def main():
             bmis[idx].append((bmi, rec_wt))
 
         male_mean = ExtractMean(bmis[0])
-        male_median = ExtractMedian(bmis[0])
+        male_deciles = ExtractDeciles(bmis[0])[1:-1]
         female_mean = ExtractMean(bmis[1])
-        female_median = ExtractMedian(bmis[1])
+        female_deciles = ExtractDeciles(bmis[0])[1:-1]
 
         both_bmis = bmis[0] + bmis[1]
         both_mean = ExtractMean(both_bmis)
-        both_median = ExtractMedian(both_bmis)
+        both_deciles = ExtractDeciles(both_bmis)[1:-1]
 
+        
+        
         print '\t'.join([str(x) for x in
           [year,
-           both_mean, both_median,
-           male_mean, male_median,
-           female_mean, female_median]])
+           both_mean] + both_deciles + \
+           [male_mean] + male_deciles +  \
+           [female_mean] + female_deciles])
 
 
 if __name__ == "__main__":
